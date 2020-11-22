ad668f739b101e99523d3e32395c9ed2281f3ff8
Viktor <viktor.fonic@gmail.com>
2020-11-22 17:49:02 +0100

    Refactor Goals FormModal to functional component

--- app/javascript/components/goals/FormModal.jsx
+++ app/javascript/components/goals/FormModal.jsx
@@ -1,65 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Modal from '../shared/Modal'
-import CurrencyInput from '../shared/CurrencyInput'
-import FieldErrors from '../shared/FieldErrors'
-import { Goals } from '../../api/main'
-import { Alerts } from '../../helpers/main'
+import React, { useState } from "react"
+import Modal from "../shared/Modal"
+import CurrencyInput from "../shared/CurrencyInput"
+import { Goals } from "../../api/main"
+import { Alerts } from "../../helpers/main"
 
-class FormModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      goal: this.props.goals.monthly || 0,
-      errors: {},
-      submitted: false,
-    };
-  }
+const FormModal = ({ onClose, onSave, goals = { monthly: 0 } }) => {
+	const [goal, setGoal] = useState(goals.monthly)
 
-  handleGoalChange = (num) => { this.setState({ goal: num }) }
-  handleErrors = (key, errs) => { this.setState({ errors: Object.assign(this.state.errors, { [key]: errs }) }); }
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.setState({ submitted: true });
-    if (Object.values(this.state.errors).flat().length) { return; }
+	const handleGoalChange = num => setGoal(num)
 
-    Goals.update({ monthly_goal: this.state.goal }).then(
-      (resp) => { this.props.onSave(resp) },
-      (error) => { Alerts.genericError(); },
-    )
-  }
+	const handleSubmit = e => {
+		e.preventDefault()
 
-  render() {
-    return (
-      <Modal title="Edit Goal" onClose={this.props.onClose}>
-        <form onSubmit={this.handleSubmit}>
-          <div className="row">
-            <div className="input-group">
-              <label>Total Monthly Goal</label>
-              <CurrencyInput initialValue={this.state.goal} onChange={this.handleGoalChange} />
-            </div>
-          </div>
+		Goals.update({ monthly_goal: goal }).then(
+			resp => onSave(resp),
+			() => Alerts.genericError()
+		)
+	}
 
-          <div className="form-actions">
-            <button type="submit" className="btn btn-dark">Save</button>
-          </div>
-        </form>
-      </Modal>
-    );
-  }
+	return (
+		<Modal title="Edit Goal" onClose={onClose}>
+			<form onSubmit={handleSubmit}>
+				<div className="row">
+					<div className="input-group">
+						<label>Total Monthly Goal</label>
+						<CurrencyInput initialValue={goal} onChange={handleGoalChange} />
+					</div>
+				</div>
+
+				<div className="form-actions">
+					<button type="submit" className="btn btn-dark">
+						Save
+					</button>
+				</div>
+			</form>
+		</Modal>
+	)
 }
 
-FormModal.defaultProps = {
-  colorsToSkip: [],
-  goals: {
-    monthly: 0,
-  }
-}
-
-FormModal.propTypes = {
-  onClose: PropTypes.func,
-  onSave: PropTypes.func,
-  goals: PropTypes.object,
-}
-
-export default FormModal;
+export default FormModal
